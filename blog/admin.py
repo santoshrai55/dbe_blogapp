1b34359d0b68fad7e800d48b6a8a284fe09aea8c
Santosh Rai <rai.santosh55@gmail.com>
2022-10-01 09:20:13 +0545

    author fields will be dasplayed with lookup widget

--- blog/admin.py
+++ blog/admin.py
@@ -9,3 +9,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author')
