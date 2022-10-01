5990286f7caf0098b3ec7b2f459643065b1e6e2a
Santosh Rai <rai.santosh55@gmail.com>
2022-10-01 09:22:37 +0545

    displays date hierarchy in admin pannel

--- blog/admin.py
+++ blog/admin.py
@@ -9,4 +9,5 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
