bbf02e0a9873cf87a21a8188847d2d96ce4f6105
Santosh Rai <rai.santosh55@gmail.com>
2022-10-01 09:16:33 +0545

    added search field in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -7,3 +7,4 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
