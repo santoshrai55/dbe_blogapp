2a721c8627eef2ff10105e3c59b32cd84e398c84
Santosh Rai <rai.santosh55@gmail.com>
2022-10-22 09:00:27 +0545

    created post share view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 # Create your views here.
 
 
@@ -13,3 +14,20 @@ def post_detail(request, year, month, day, post):
                              publish__year=year, publish__month=month, publish__day=day)
 
     return render(request, 'blog/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # retrives post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+
+            # ... send email
+
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/share.html', {'post': post, 'form': form})
